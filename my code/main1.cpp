e272dcf9541726103ecfecca8de026cfdc83e67a
PHS257229 <phs257229@physics.iitd.ac.in>
2026-08-20 05:20:18 +0000

    main1

---
+++ my code/main1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row;
+    int col;
+    cout<<"Enter the row no : ";
+    cin>>row;
+    cout<<"Enter the col no : ";
+    cin>>col;
+
+    
+
+    return 0;
+}
